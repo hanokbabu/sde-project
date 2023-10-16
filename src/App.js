96a97c66c30a442bc315225c7a6e38ab8a60325b
hanokbabu <100519430+hanokbabu@users.noreply.github.com>
2023-10-16 18:37:49 +1100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import NavBar from './NavBar.js';
+import ImageList from './ImageList.js';
+import Bottom from './Bottom';
+
+function App() {
+  return (
+    <>
+      <NavBar/>
+      <ImageList/>
+      <Bottom/>
+    </>
+  );
+}
+
+export default App;
